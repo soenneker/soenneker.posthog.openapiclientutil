e2b3fb0de5d382d47d07ff63f39acdcaf51c6d1c
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:36 -0500

    xml commenting

--- src/Soenneker.PostHog.OpenApiClientUtil/Abstract/IPostHogOpenApiClientUtil.cs
+++ src/Soenneker.PostHog.OpenApiClientUtil/Abstract/IPostHogOpenApiClientUtil.cs
@@ -10,5 +10,10 @@ namespace Soenneker.PostHog.OpenApiClientUtil.Abstract;
 /// </summary>
 public interface IPostHogOpenApiClientUtil: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Gets the value.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task containing the result of the operation.</returns>
     ValueTask<PostHogOpenApiClient> Get(CancellationToken cancellationToken = default);
 }
